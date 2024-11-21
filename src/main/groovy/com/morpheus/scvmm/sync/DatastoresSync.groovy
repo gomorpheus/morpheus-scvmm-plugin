b362c476b9e25cca35c5a2dcec5cfc2bdd5f6a35
rahulray19 <100296364+rahulray19@users.noreply.github.com>
2024-11-21 15:31:07 +0530

    scvmm plugin: cacheDatastores

--- src/main/groovy/com/morpheus/scvmm/sync/DatastoresSync.groovy
+++ src/main/groovy/com/morpheus/scvmm/sync/DatastoresSync.groovy
@@ -7,6 +7,7 @@ import com.morpheusdata.core.util.SyncTask
 import com.morpheusdata.model.*
 import com.morpheusdata.model.projection.DatastoreIdentityProjection
 import groovy.util.logging.Slf4j
+import io.reactivex.rxjava3.core.Observable
 
 /**
  * @author rahul.ray
@@ -30,102 +31,74 @@ class DatastoresSync {
     def execute() {
         log.debug "DatastoresSync"
         try {
-            log.info ("Ray :: DatastoresSync: cloud.id: ${cloud.id}")
-            def clusters = context.services.cloud.pool.list(new DataQuery()
+            List<CloudPool> clusters = context.services.cloud.pool.list(new DataQuery()
                     .withFilter('refType', 'ComputeZone').withFilter('refId', cloud.id)
-                    .withFilter('type', 'Cluster')
-                    .withFilter('internalId', '!=', null))
-            log.info ("Ray :: DatastoresSync: clusters: ${clusters}")
-            log.info ("Ray :: DatastoresSync: clusters?.size(): ${clusters?.size()}")
-            def volumeType = context.services.storageVolume.storageVolumeType.find(new DataQuery()
-                    .withFilter('code', 'scvmm-datastore'))
-            log.info ("Ray :: DatastoresSync: volumeType: ${volumeType}")
+                    .withFilter('type', 'Cluster').withFilter('internalId', '!=', null))
+            StorageVolumeType volumeType = context.services.storageVolume.storageVolumeType.find(new DataQuery().withFilter('code', 'scvmm-datastore'))
             def scvmmOpts = apiService.getScvmmZoneAndHypervisorOpts(context, cloud, node)
-            log.info ("Ray :: DatastoresSync: scvmmOpts: ${scvmmOpts}")
             def listResults = apiService.listDatastores(scvmmOpts)
-            log.info ("Ray :: DatastoresSync: listResults: ${listResults}")
-            log.info ("Ray :: DatastoresSync: listResults.success: ${listResults.success}")
-            log.info ("Ray :: DatastoresSync: listResults.datastores: ${listResults.datastores}")
+            log.debug("DatastoresSync: listResults: ${listResults}")
             if (listResults.success == true && listResults.datastores) {
-                def existingHosts = context.services.computeServer.list(new DataQuery()
-                        .withFilter('zone.id', cloud.id)
+                def objList = []
+                def partitionUniqueIds = []
+                listResults.datastores?.each { data ->
+                    if (!partitionUniqueIds?.contains(data.partitionUniqueID)) {
+                        objList << data
+                    }
+                    partitionUniqueIds << data.partitionUniqueID
+                }
+                log.debug("DatastoresSync: objList: ${objList}")
+                List<ComputeServer> existingHosts = context.services.computeServer.list(new DataQuery().withFilter('zone.id', cloud.id)
                         .withFilter('computeServerType.code', 'scvmmHypervisor'))
-                log.info ("Ray :: DatastoresSync: existingHosts: ${existingHosts}")
-                log.info ("Ray :: DatastoresSync: existingHosts?.size(): ${existingHosts?.size()}")
 
-                def existingItems = context.async.cloud.datastore.listIdentityProjections(new DataQuery()
+                Observable<DatastoreIdentityProjection> existingItems = context.async.cloud.datastore.listIdentityProjections(new DataQuery()
                         .withFilter('category', '=~', 'scvmm.datastore.%').withFilter('refType', 'ComputeZone')
                         .withFilter('refId', cloud.id).withFilter('type', 'generic'))
-                log.info ("Ray :: DatastoresSync: existingItems: ${existingItems}")
 
-                SyncTask<DatastoreIdentityProjection, Map, Datastore> syncTask = new SyncTask<>(existingItems, listResults?.datastores as Collection<Map>)
-                log.info ("Ray :: DatastoresSync: syncTask: ${syncTask}")
-                // check: deprication
+                SyncTask<DatastoreIdentityProjection, Map, Datastore> syncTask = new SyncTask<>(existingItems, objList as Collection<Map>)
                 syncTask.addMatchFunction { existingItem, cloudItem ->
-                    log.info ("=================================================Ray :: start >>> DatastoresSync>>addMatchFunction:=================================================")
-                    def id = getDataStoreExternalId(cloudItem)
-                    log.info ("Ray :: DatastoresSync>>addMatchFunction: existingItem.externalId: ${existingItem.externalId?.toString()}")
-                    log.info ("Ray :: DatastoresSync>>addMatchFunction: id: ${getDataStoreExternalId(cloudItem)?.toString()}")
-                    existingItem.externalId?.toString() == id?.toString()
-                    log.info ("=================================================Ray :: end >>> DatastoresSync>>addMatchFunction:=================================================")
+                    existingItem.externalId?.toString() == cloudItem.partitionUniqueID?.toString()
                 }.onDelete { removeItems ->
-                    log.info ("Ray :: DatastoresSync>>onDelete: removeItems: ${removeItems}")
-                    log.info ("Ray :: DatastoresSync>>onDelete: removeItems?.size(): ${removeItems?.size()}")
                     removeMissingDatastores(removeItems)
                 }.onUpdate { updateItems ->
-                    log.info ("Ray :: DatastoresSync>>onUpdate: updateItems: ${updateItems}")
                     updateMatchedDatastores(updateItems, clusters, existingHosts, volumeType)
                 }.onAdd { itemsToAdd ->
-                    log.info ("Ray :: DatastoresSync>>onAdd: itemsToAdd: ${itemsToAdd}")
                     addMissingDatastores(itemsToAdd, clusters, existingHosts, volumeType)
-                }.withLoadObjectDetailsFromFinder { updateItems ->
+                }.withLoadObjectDetailsFromFinder { List<SyncTask.UpdateItemDto<DatastoreIdentityProjection, Map>> updateItems ->
                     return context.async.cloud.datastore.listById(updateItems.collect { it.existingItem.id } as List<Long>)
                 }.start()
             }
         } catch (ex) {
-            log.error("Ray :: DatastoresSync error: {}", ex.getMessage())
+            log.error("DatastoresSync error: {}", ex.getMessage())
         }
     }
 
-    private removeMissingDatastores(Collection<DatastoreIdentityProjection> removeList) {
+    private removeMissingDatastores(List<DatastoreIdentityProjection> removeList) {
         log.debug("removeMissingDatastores: ${cloud} ${removeList.size()}")
-        log.info ("Ray :: removeMissingDatastores: ${cloud} ${removeList.size()}")
         try {
-            context.async.cloud.datastore.remove(removeList).blockingGet()
+            context.services.cloud.datastore.bulkRemove(removeList)
         } catch (ex) {
-            log.error("Ray :: DatastoresSync>>removeMissingDatastores error: {}", ex.getMessage())
+            log.error("DatastoresSync: removeMissingDatastores error: {}", ex.getMessage())
         }
     }
 
     private void updateMatchedDatastores(List<SyncTask.UpdateItem<Datastore, Map>> updateList, clusters, existingHosts, volumeType) {
         try {
             log.debug("updateMatchedDatastores: ${updateList?.size()}")
-            log.info ("Ray :: updateMatchedDatastores: clusters?.size(): ${clusters?.size()}")
-            log.info ("Ray :: updateMatchedDatastores: existingHosts?.size(): ${existingHosts?.size()}")
-            log.info ("Ray :: updateMatchedDatastores: volumeType: ${volumeType}")
             def host
             updateList.each { item ->
                 def masterItem = item.masterItem
-                log.info ("Ray :: updateMatchedDatastores: masterItem: ${masterItem}")
                 def existingItem = item.existingItem
-                log.info ("Ray :: updateMatchedDatastores: existingItem: ${existingItem}")
-                log.info ("Ray :: updateMatchedDatastores: masterItem.vmHost: ${masterItem.vmHost}")
                 host = existingHosts.find { it.hostname == masterItem.vmHost }
-                log.info ("Ray :: updateMatchedDatastores: host: ${host}")
-                log.info ("Ray :: updateMatchedDatastores: host?.id: ${host?.id}")
                 def cluster
-                log.info ("Ray :: updateMatchedDatastores: masterItem.isClusteredSharedVolume: ${masterItem.isClusteredSharedVolume}")
-                log.info ("Ray :: updateMatchedDatastores: masterItem.name: ${masterItem.name}")
                 if (masterItem.isClusteredSharedVolume) {
                     cluster = clusters.find { c ->
-                        c.getConfigProperty('sharedVolumes')?.contains(masterItem.name) && (!host || host.resourcePool.id == c.id)
+                        c.getConfigProperty('sharedVolumes')?.toString().contains(masterItem.name) && (!host || host.resourcePool.id == c.id)
                     }
                 }
                 def name = getName(masterItem, cluster, host)
-                log.info ("Ray :: updateMatchedDatastores: name: ${name}")
                 def doSave = false
 
-                log.info ("Ray :: updateMatchedDatastores: masterItem.isAvailableForPlacement: ${masterItem.isAvailableForPlacement}")
                 if (existingItem.online != masterItem.isAvailableForPlacement) {
                     existingItem.online = masterItem.isAvailableForPlacement
                     doSave = true
@@ -135,13 +108,11 @@ class DatastoresSync {
                     doSave = true
                 }
                 def freeSpace = masterItem.freeSpace?.toLong() ?: 0
-                log.info ("Ray :: updateMatchedDatastores: freeSpace: ${freeSpace}")
                 if (existingItem.freeSpace != freeSpace) {
                     existingItem.freeSpace = freeSpace
                     doSave = true
                 }
                 def totalSize = masterItem.size ? masterItem.size?.toLong() : masterItem.capacity ? masterItem.capacity?.toLong() : 0
-                log.info ("Ray :: updateMatchedDatastores: totalSize: ${totalSize}")
                 if (existingItem.storageSize != totalSize) {
                     existingItem.storageSize = totalSize
                     doSave = true
@@ -150,43 +121,31 @@ class DatastoresSync {
                     existingItem.zonePool = cluster
                     doSave = true
                 }
-                log.info ("Ray :: updateMatchedDatastores: doSave: ${doSave}")
                 if (doSave) {
                     def savedDataStore = context.async.cloud.datastore.save(existingItem).blockingGet()
-                    log.info ("Ray :: updateMatchedDatastores: savedDataStore: ${savedDataStore}")
-                    log.info ("Ray :: updateMatchedDatastores: savedDataStore?.id: ${savedDataStore?.id}")
                     if (savedDataStore && host) {
-                        log.info ("Ray :: updateMatchedDatastores: before calling syncVolume....")
                         syncVolume(masterItem, host, savedDataStore, volumeType, getDataStoreExternalId(masterItem))
-                        log.info ("Ray :: updateMatchedDatastores: after calling syncVolume....")
                     }
                 }
             }
         } catch (e) {
-            log.error("Ray :: Error in updateMatchedDatastores method: ${e}", e)
+            log.error("Error in updateMatchedDatastores method: ${e}", e)
         }
     }
 
     private addMissingDatastores(Collection<Map> addList, clusters, existingHosts, volumeType) {
-        log.debug("IsolationNetworkSync >> addMissingNetworks >> called")
-        log.info ("Ray :: addMissingDatastores: addList?.size(): ${addList?.size()}")
+        log.debug("addMissingDatastores: addList?.size(): ${addList?.size()}")
         try {
             addList?.each { Map item ->
                 def isSharedVolume = item.isClusteredSharedVolume
-                log.info ("Ray :: addMissingDatastores: isSharedVolume: ${isSharedVolume}")
                 def externalId = getDataStoreExternalId(item)
-                log.info ("Ray :: addMissingDatastores: externalId: ${externalId}")
-                log.info ("Ray :: addMissingDatastores: item.vmHost: ${item.vmHost}")
                 ComputeServer host = existingHosts.find { it.hostname == item.vmHost }
-                log.info ("Ray :: addMissingDatastores: host: ${host}")
-                log.info ("Ray :: addMissingDatastores: host?.id: ${host?.id}")
                 def cluster
                 if (isSharedVolume) {
                     cluster = clusters.find { c ->
-                        c.getConfigProperty('sharedVolumes')?.contains(item.name) && (!host || host.resourcePool.id == c.id)
+                        c.getConfigProperty('sharedVolumes')?.toString().contains(item.name) && (!host || host.resourcePool.id == c.id)
                     }
                 }
-                log.info ("Ray :: addMissingDatastores: cluster: ${cluster}")
                 def datastoreConfig =
                         [
                                 cloud      : cloud,
@@ -204,41 +163,27 @@ class DatastoresSync {
                                 active     : cloud.defaultDatastoreSyncActive,
                                 storageSize: item.size ? item.size?.toLong() : item.capacity ? item.capacity?.toLong() : 0
                         ]
-                log.info ("Ray :: addMissingDatastores: datastoreConfig: ${datastoreConfig}")
-                log.info "Created datastore for id: ${externalId}"
+                log.debug("datastoreConfig: ${datastoreConfig}")
                 Datastore datastore = new Datastore(datastoreConfig)
                 def savedDataStore = context.async.cloud.datastore.create(datastore).blockingGet()
-                log.info ("Ray :: addMissingDatastores: savedDataStore: ${savedDataStore}")
-                log.info ("Ray :: addMissingDatastores: savedDataStore?.id: ${savedDataStore?.id}")
+                log.debug("savedDataStore?.id: ${savedDataStore?.id}")
                 if (savedDataStore && host) {
-                    log.info ("Ray :: addMissingDatastores: before calling syncVolume...")
                     syncVolume(item, host, savedDataStore, volumeType, externalId)
-                    log.info ("Ray :: addMissingDatastores: after calling syncVolume...")
                 }
             }
         } catch (e) {
-            log.error "Ray :: Error in adding Datastores sync ${e}", e
+            log.error "Error in adding Datastores sync ${e}", e
         }
     }
 
     private syncVolume(item, ComputeServer host, savedDataStore, volumeType, externalId) {
-        log.info ("Ray :: syncVolume: item: ${item}")
-        log.info ("Ray :: syncVolume: host: ${host}")
-        log.info ("Ray :: syncVolume: savedDataStore: ${savedDataStore}")
-        log.info ("Ray :: syncVolume: volumeType: ${volumeType}")
-        log.info ("Ray :: syncVolume: externalId: ${externalId}")
         try {
             // See if the volume is attached to the server yet
             def totalSize = item.size ? item.size?.toLong() : item.capacity ? item.capacity?.toLong() : 0
-            log.info ("Ray :: syncVolume: totalSize: ${totalSize}")
             def freeSpace = item.freeSpace?.toLong() ?: 0
-            log.info ("Ray :: syncVolume: freeSpace: ${freeSpace}")
             def usedSpace = totalSize - freeSpace
-            log.info ("Ray :: syncVolume: usedSpace: ${usedSpace}")
             def mountPoint = item.mountPoints?.size() > 0 ? item.mountPoints[0] : null
-            log.info ("Ray :: syncVolume: mountPoint: ${mountPoint}")
             StorageVolume existingVolume = host.volumes.find { it.externalId == externalId }
-            log.info ("Ray :: syncVolume: existingVolume: ${existingVolume}")
             if (existingVolume) {
                 def save = false
                 if (existingVolume.internalId != item.id) {
@@ -259,15 +204,14 @@ class DatastoresSync {
                     existingVolume.name = item.name
                     save = true
                 }
-                /*if(existingVolume.volumePath != mountPoint){ // check: volumePath does not exist in StorageVolume
+                if (existingVolume.volumePath != mountPoint) { // check: volumePath does not exist in StorageVolume
                     existingVolume.volumePath = mountPoint
                     save = true
-                }*/
+                }
                 if (existingVolume.datastore?.id != savedDataStore.id) {
                     existingVolume.datastore = savedDataStore
                     save = true
                 }
-                log.info ("Ray :: syncVolume: save: ${save}")
                 if (save) {
                     context.async.storageVolume.save(existingVolume).blockingGet()
                 }
@@ -279,30 +223,26 @@ class DatastoresSync {
                         maxStorage: totalSize,
                         usedStorage: usedSpace,
                         externalId: externalId,
-                        internalId: item.id
-                        //volumePath:mountPoint, // check:
+                        internalId: item.id,
+                        volumePath: mountPoint
                 )
-                log.info ("Ray :: syncVolume: newVolume: ${newVolume}")
                 newVolume.datastore = savedDataStore
-                //host.addToVolumes(newVolume)
-                //newVolume.save(flush:true,failOnError:true)
-                //host.save(flush:true)
                 context.async.storageVolume.create([newVolume], host).blockingGet()
-                log.info ("Ray :: syncVolume: newVolume created")
+                log.debug("syncVolume: newVolume created")
             }
 
         } catch (ex) {
-            log.error "Ray :: Error in volumeSync Datastores ${ex}", ex
+            log.error "Error in volumeSync Datastores ${ex}", ex
         }
     }
 
-    private getDataStoreExternalId(ds) {
-        if (ds.partitionUniqueID) {
-            return ds.partitionUniqueID
-        } else if (ds.isClusteredSharedVolume && ds.storageVolumeID) {
-            return ds.storageVolumeID
+    private getDataStoreExternalId(cloudItem) {
+        if (cloudItem.partitionUniqueID) {
+            return cloudItem.partitionUniqueID
+        } else if (cloudItem.isClusteredSharedVolume && cloudItem.storageVolumeID) {
+            return cloudItem.storageVolumeID
         } else {
-            return "${ds.name}|${ds.vmHost}"
+            return "${cloudItem.name}|${cloudItem.vmHost}"
         }
     }
 
@@ -315,5 +255,4 @@ class DatastoresSync {
         }
         return name
     }
-
 }
