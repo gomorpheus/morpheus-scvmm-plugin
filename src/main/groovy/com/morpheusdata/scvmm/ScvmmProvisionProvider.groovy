91d81bfb4986f672ee9aa2a4224e6805907cfc37
moukthika-hpe <146430803+moukthika-hpe@users.noreply.github.com>
2025-07-23 11:18:58 +0530

    indentation

--- src/main/groovy/com/morpheusdata/scvmm/ScvmmProvisionProvider.groovy
+++ src/main/groovy/com/morpheusdata/scvmm/ScvmmProvisionProvider.groovy
@@ -1756,7 +1756,7 @@ class ScvmmProvisionProvider extends AbstractProvisionProvider implements Worklo
                 virtualImage = context.services.virtualImage.get(virtualImageId)
                 imageId = virtualImage.externalId
             } else {
-				virtualImage = new VirtualImage(code: 'scvmm.image.morpheus.ubuntu.22.04.20230918.amd64')
+		virtualImage = new VirtualImage(code: 'scvmm.image.morpheus.ubuntu.22.04.20230918.amd64')
                 //better this later
             }
 
